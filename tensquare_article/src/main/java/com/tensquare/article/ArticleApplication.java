2f8806fa7c4ad282146816fb24b91db74a5895a9
李敢敢 <1336976407@qq.com>
2020-09-04 22:17:18 +0800

    this is tensquare utils_tools!

---
+++ tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -0,0 +1,16 @@
+package com.tensquare.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ligangan
+ * @Date: 2020/9/4
+ * @Time: 22:14
+ */
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class,args);
+    }
+}
